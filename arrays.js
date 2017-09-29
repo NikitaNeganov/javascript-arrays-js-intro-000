aa616c6b61d0b1e72063139f9b27456eb117bc98
Nikita Neganov <nikitaneganov@icloud.com>
2017-09-29 12:32:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
   var newArray = array.unshift(element);
-  //return newArray;
+  return newArray;
 };
 function destructivelyAddElementToBeginningOfArray(array, element){
   array = array.unshift(element);
+  return array;
 };
