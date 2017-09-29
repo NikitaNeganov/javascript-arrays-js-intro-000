d39a5f83ab0528e4aaaa94f571062a14f9b925a0
Nikita Neganov <nikitaneganov@icloud.com>
2017-09-29 12:56:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,7 @@ function addElementToEndOfArray(array, element){
   var newArray = [...array, element];
   return newArray;
 }
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
